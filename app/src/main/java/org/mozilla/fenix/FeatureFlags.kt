834d5f3399c8dba33c72e7b963316602f5d794b4
forworldm <forworldm@gmail.com>
2021-07-13 14:35:01 +0800

    disable recent saved bookmarks

--- app/src/main/java/org/mozilla/fenix/FeatureFlags.kt
+++ app/src/main/java/org/mozilla/fenix/FeatureFlags.kt
@@ -47,5 +47,5 @@ object FeatureFlags {
     /**
      * Enables the recently saved bookmarks feature in the home screen.
      */
-    val recentBookmarksFeature = Config.channel.isNightlyOrDebug
+    val recentBookmarksFeature = false
 }
